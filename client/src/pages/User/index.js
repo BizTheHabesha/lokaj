310cf918d6f50c766817db52ad6d1f215a9e8e89
Biz <github@bisrat.dev>
2023-08-01 12:46:23 -0600

    add user page

---
+++ client/src/pages/User/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PageContent from "../../components/PageContent";
+
+function User() {
+	return (
+		<PageContent>
+			<h1>PAGE FOR THE USER LOL</h1>
+		</PageContent>
+	);
+}
