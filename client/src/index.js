9359b8a9bcf37a3110a2795f33d9fa21a63598f8
Biz <github@bisrat.dev>
2023-08-02 00:28:59 -0600

    add chakra theme handling [broken]

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+// import "./index.css";
+import "./App.css";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import theme from "./utils/theme";
+
+// const theme = extendTheme({
+// 	config: {
+// 		initialColorMode: "dark",
+// 		// useSystemColorMode: false,
+// 	},
+// });
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
 	</React.StrictMode>,
