7e28e397c77071313e05c6a5fddcd810f591e1ac
Biz <github@bisrat.dev>
2023-08-02 00:27:20 -0600

    add tooltips

--- client/src/pages/Navbar/index.js
+++ client/src/pages/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tabs, TabList, Tab, TabPanel, TabPanels } from "@chakra-ui/react";
+import { Tabs, TabList, Tab, Tooltip } from "@chakra-ui/react";
 import {
 	AiOutlineHome,
 	AiOutlineCar,
@@ -14,25 +14,36 @@ function Navbar(props) {
 			defaultIndex={props.active}
 			isFitted
 			variant="soft-rounded"
+			colorScheme={props.defaultScheme}
 			size={"lg"}
 			pt={4}
 			px={3}>
 			<TabList mb="1em">
-				<Tab px={1} as={"a"} href="/">
-					<AiOutlineHome size={30} />
-				</Tab>
-				<Tab px={1} as={"a"} href="/carlog">
-					<AiOutlineCar size={30} />
-				</Tab>
-				<Tab px={1} as={"a"} href="/payroll">
-					<GrDocumentUser size={30} />
-				</Tab>
-				<Tab px={1} as={"a"} href="/reports">
-					<HiOutlineDocumentReport size={30} />
-				</Tab>
-				<Tab px={1} as={"a"} href="/clock">
-					<AiOutlineClockCircle size={30} />
-				</Tab>
+				<Tooltip label="Home">
+					<Tab px={1} as={"a"} href="/">
+						<AiOutlineHome size={30} />
+					</Tab>
+				</Tooltip>
+				<Tooltip label="Car Log">
+					<Tab px={1} as={"a"} href="/carlog">
+						<AiOutlineCar size={30} />
+					</Tab>
+				</Tooltip>
+				<Tooltip label="Payroll">
+					<Tab px={1} as={"a"} href="/payroll">
+						<GrDocumentUser size={30} />
+					</Tab>
+				</Tooltip>
+				<Tooltip label="Reports">
+					<Tab px={1} as={"a"} href="/reports">
+						<HiOutlineDocumentReport size={30} />
+					</Tab>
+				</Tooltip>
+				<Tooltip label="Time Clock">
+					<Tab px={1} as={"a"} href="/clock">
+						<AiOutlineClockCircle size={30} />
+					</Tab>
+				</Tooltip>
 			</TabList>
 		</Tabs>
 	);
