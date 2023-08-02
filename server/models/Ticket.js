692a3193e3dd24876e6eef833315cfa4ae28aec5
Joseph Teklu <jteklu@outlook.com>
2023-08-01 22:03:42 -0600

    added connection to db, auth for users and server for opening port, partial done resolvers and typedefs

--- server/models/Ticket.js
+++ server/models/Ticket.js
@@ -54,17 +54,18 @@ const ticketSchema = new Schema({
 		type: String,
 		default: "In",
 		validate: function (data) {
-			return ["In", "Out", "Checked Out"].find(data);
+			return ["In", "Out", "Checked Out"].includes(data);
 		},
 	},
 	type: {
 		type: String,
 		required: true,
 		validate: function (data){
-			return ["OvernightValet", "OvernightSelf", "DailyValet", "DailySelf"]
-		}
+			return ["OvernightValet", "OvernightSelf", "DailyValet", "DailySelf"].includes(data);
+		},
+		
 	},
-	damageCheck: [{ type: Number, default: [] }],
+	damageCheck: [{ type: Number, default: [], ref: "damageCheck" }],
 });
 
 const Ticket = model("Ticket", ticketSchema);
--- server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const ticketRoutes = require("./ticketRoutes");
-const userRoutes = require("./userRoutes");
-
-router.use("/ticket", ticketRoutes);
-router.use("/user", userRoutes);
-
-module.exports = router;
