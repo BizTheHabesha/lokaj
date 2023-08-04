23c6116fc79ed2b56c8b19118e0a2c6fab743f8f
Joseph Teklu <jteklu@outlook.com>
2023-08-04 15:54:44 -0600

    worked on queries and mutations in client

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,52 @@
 import { gql } from "@apollo/client";
+
+export const QUERY_ALL_USERS = gql `
+    {
+        users {
+            _id
+            username
+            lastName
+            firstName
+            internalRef
+            position
+            password
+        }
+    }
+`;
+
+export const QUERY_USER = gql `
+    query getUser($ticket: ticketId){
+        tickets(ticket: $ticket) {
+            username
+            lastName
+            firstName
+            internalRef
+            position
+            password
+        }
+    }
+`;
+
+export const QUERY_TICKET = gql `
+    query getTickets($ticket: ticketId){
+        tickets(ticket: $ticket) {
+            _id
+            ticketId 
+            lastName 
+            firstName 
+            room 
+            checkIn 
+            checkOut 
+            vehicleMake 
+            vehicleModel 
+            vehicleColor 
+            vehiclePlate 
+            vehicleLocation
+            lastRunner 
+            status 
+            type 
+            damageCheck 
+            comment 
+        }
+    }
+`;
