17b800a0803363a4d389d81fbe6d7bd5324c4a95
Biz <github@bisrat.dev>
2023-08-06 00:33:14 -0600

    add react 18 render method [unimplemented]

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom";
-// import "./index.css";
 import "./App.css";
 import App from "./App";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./utils/theme";
 
-// const theme = extendTheme({
-// 	config: {
-// 		initialColorMode: "dark",
-// 		// useSystemColorMode: false,
-// 	},
-// });
-
 ReactDOM.render(
-	<React.StrictMode>
+	<StrictMode>
 		<ChakraProvider theme={theme}>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 			<App />
 		</ChakraProvider>
-	</React.StrictMode>,
+	</StrictMode>,
 	document.getElementById("root")
 );
+
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(
+// 	<StrictMode>
+// 		<ChakraProvider theme={theme}>
+// 			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+// 			<App />
+// 		</ChakraProvider>
+// 	</StrictMode>
+// );
