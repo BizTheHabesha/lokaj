a63bfba666d13655852ff52f115cde7449635b7a
Biz <github@bisrat.dev>
2023-08-26 00:10:22 -0600

    fix user time card styling

--- client/src/pages/Payroll/index.js
+++ client/src/pages/Payroll/index.js
@@ -94,7 +94,7 @@ function Payroll(props) {
 											? "Clocked In"
 											: ""
 									}>
-									<Card>
+									<Card w={"80%"}>
 										<CardHeader>
 											<HStack>
 												<Heading size="md">
